86a2304dbb54810019036390f70cc649ddff08e7
khawla <wanteed5.k@gmail.com>
2019-01-22 15:00:34 +0400

    V1

--- Pets B&B/ViewController.swift
+++ Pets B&B/ViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import LFLoginController
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,LFLoginControllerDelegate {
+    
+    func loginDidFinish(email: String, password: String, type: LFLoginController.SendType) {
+        
+    }
+    
+    func forgotPasswordTapped(email: String) {
+        
+    }
+    
 
+    let LFcontroller = LFLoginController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      LFcontroller.delegate = self
+        
+        // Customizations
+        LFcontroller.logo = UIImage(named: "AwesomeLabsLogoWhite")
+        LFcontroller.isSignupSupported = false
+        LFcontroller.backgroundColor = UIColor(red: 224 / 255, green: 68 / 255, blue: 98 / 255, alpha: 1)
+        LFcontroller.videoURL = Bundle.main.url(forResource: "CD", withExtension: "mov")?.absoluteURL
+        LFcontroller.loginButtonColor = UIColor.purple
+        //        controller.setupOnePassword("YourAppName", appUrl: "YourAppURL")
+        
+        
     }
-
-
+    
+    @IBAction func butLoginTapped(_ sender: UIButton) {
+        self.navigationController?.pushViewController(LFcontroller, animated: true)
+    }
 }
+
+
+//extension ViewController: LFLoginControllerDelegate {
+//
+//    func loginDidFinish(email: String, password: String, type: LFLoginController.SendType) {
+//
+//        // Implement your server call here
+//
+//        print(email)
+//        print(password)
+//        print(type)
+//
+//        // Example
+//        if type == .Login && password != "1234" {
+//
+//            controller.wrongInfoShake()
+//        } else {
+//
+//            _ = navigationController?.popViewController(animated: true)
+//        }
+//    }
+//
+//    func forgotPasswordTapped(email: String) {
+//        print("forgot password: \(email)")
+//    }
+//
+//}
 
